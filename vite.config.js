63c65f0994cb7c2754ae005cdfdc7fb65667117e
TejaswitaSingh <tejaswitasinghtanwar@gmail.com>
2025-02-15 09:54:54 +0530

    Fixed GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Docs-Framer/", // Add your repo name here (case-sensitive)
-});
+  base: "/Docs-Framer/", // Make sure this matches your repo name exactly!
+})
